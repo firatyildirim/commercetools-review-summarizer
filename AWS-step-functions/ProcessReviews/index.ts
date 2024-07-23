45be18babb8db4b3e9fe4bcb1864429417003407
Kaan Özçakmak <k.ozcakmak@emakina.com.tr>
2024-07-23 13:02:49 +0300

    GPT config fixed

--- AWS-step-functions/ProcessReviews/index.ts
+++ AWS-step-functions/ProcessReviews/index.ts
@@ -31,15 +31,31 @@ What it does: The System role is used to provide setup information or context th
 When to use it: You use this role to set the stage for the interaction. For example, if you want the model to maintain a formal tone throughout the conversation or if you need to specify rules like avoiding certain topics.
 */
 
-export const handler: APIGatewayProxyHandler = async (event: any) => {
+export const handler = async (event: any, context: any) => {
   if(event == null) return;
-  const reviews = JSON.parse(event.body);
+  const eventBody =JSON.parse(event.body);
+  const reviews = JSON.stringify(eventBody);
   if(reviews == null) {
     return;
   }
   try {
-    var results = await SendRequestToGPT(reviews);
-    console.log(results);
+    var response =   await openai.chat.completions.create({
+		model: GPT_3_5_TURBO,
+		max_tokens:150,//to be calculated and determined
+    	temperature:1, // Use the appropriate model
+		messages: [
+		  {
+			role: USER,
+			content: reviews,
+		  },
+		  { role: SYSTEM,
+			content: SYSTEM_CONTENT2 
+		  }
+		],
+	  });
+  
+	  console.log('GPT API response:', response);
+	  return response;
     
 
   } catch (error : any) {
@@ -51,61 +67,53 @@ export const handler: APIGatewayProxyHandler = async (event: any) => {
 };
 
 
-async function SendRequestToGPT(userContent: any) {
-  const completion = await openai.chat.completions.create({
-    messages: [{ role: SYSTEM, content: SYSTEM_CONTENT },{ role: USER, content: userContent }],
-    model: GPT_3_5_TURBO,
-    max_tokens:100,//to be calculated and determined
-    temperature:1//decimal value between 0.0 and 2.0. Determines the freedom of the GPT when generating answers.
-  });
-  return completion.choices[0];
+
+const SYSTEM_CONTENT2=`You are a bot designed to process and summarize product reviews given in the form of a JSON object in a string. Given the JSON object below, you must analyze the comments to create a brief summary.
+
+Example JSON Object:
+
+[
+  {
+	"score": 4,
+	"date": "12-04-2024",
+	"comment": "Ömürlük bir ürün. İnanılmaz süreler sıcak ve soğuk tutuyor. Her sene farklı markalardan işe yaramayan termos almaktansa tek seferde buna para verin :)",
+	"locale": "tr-TR"
+  }
+]
+
+Summarize these comments into three categories: “common positive comment”, “common negative comments”, “notable observation”. The summaries shouldn't exceed 3 sentences.
+
+Translate the generated summaries to these languages [tr-TR, en-US, fr-FR, de-DE, nl-NL].
+
+Do not take any discriminative, rude and bad comments to summary as a parameter.
+
+The output must be a JSON object as shown below, with "average-score" as a floating number with two decimal points and all other values as strings:
+
+{
+	"average-score": double .2f,
+	"summary": "string",
+	"common-positive": {
+		"tr-TR": "string",
+		"en-US": "string",
+		"fr-FR": "string",
+		"de-DE": "string",
+		"nl-NL": "string"
+	},
+	"common-negative": {
+		"tr-TR": "string",
+		"en-US": "string",
+		"fr-FR": "string",
+		"de-DE": "string",
+		"nl-NL": "string"
+	},
+	"noteable-observation": {
+		"tr-TR": "string",
+		"en-US": "string",
+		"fr-FR": "string",
+		"de-DE": "string",
+		"nl-NL": "string"
+	}
 }
 
-const SYSTEM_CONTENT = "You are a bot designed to process and summarize product reviews given in the form of a JSON object. Given the JSON object below, you must analyze the comments to create a brief summary." +
-	"" +
-	"Example JSON Object:" +
-	"" +
-	"[" +
-	"  {" +
-	"	\"score\": 4," +
-	"	\"date\": \"12-04-2024\"," +
-	"	\"comment\": \"Ömürlük bir ürün. İnanılmaz süreler sıcak ve soğuk tutuyor. Her sene farklı markalardan işe yaramayan termos almaktansa tek seferde buna para verin :)\"," +
-	"	\"locale\": \"tr-TR\"" +
-	"  }" +
-	"]" +
-	"" +
-	"Summarize these comments into three categories: “common positive comment”, “common negative comments”, “notable observation”. The summaries shouldn't exceed 3 sentences." +
-	"" +
-	"Translate the generated summaries to these languages [tr-TR, en-US, fr-FR, de-DE, nl-NL]." +
-	"" +
-	"Do not take any discriminative, rude and bad comments to summary as a parameter." +
-	"" +
-	"The output must be a JSON object as shown below, with \"average-score\" as a floating number with two decimal points and all other values as strings:" +
-	"" +
-	"{" +
-	"	\"average-score\": double .2f," +
-	"	\"summary\": \"string\"," +
-	"	\"common-positive\": {" +
-	"		\"tr-TR\": \"string\"," +
-	"		\"en-US\": \"string\"," +
-	"		\"fr-FR\": \"string\"," +
-	"		\"de-DE\": \"string\"," +
-	"		\"nl-NL\": \"string\"" +
-	"	}," +
-	"	\"common-negative\": {" +
-	"		\"tr-TR\": \"string\"," +
-	"		\"en-US\": \"string\"," +
-	"		\"fr-FR\": \"string\"," +
-	"		\"de-DE\": \"string\"," +
-	"		\"nl-NL\": \"string\"" +
-	"	}," +
-	"	\"noteable-observation\": {" +
-	"		\"tr-TR\": \"string\"," +
-	"		\"en-US\": \"string\"," +
-	"		\"fr-FR\": \"string\"," +
-	"		\"de-DE\": \"string\"," +
-	"		\"nl-NL\": \"string\"" +
-	"	}" +
-	"}" +
-	"" +
-	"Remember, the output must be a valid JSON object.";
+Remember, the output must be a valid JSON object like stated above.`
+
